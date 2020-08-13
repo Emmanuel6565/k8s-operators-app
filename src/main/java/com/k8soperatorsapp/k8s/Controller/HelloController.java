2d4190ee963971d69262e6c084042f108efb66b0
Emmanuel6565 <emmanuelassamoi65@gmail.com>
2020-08-13 14:52:21 +0200

    java spring boot

--- src/main/java/com/k8soperatorsapp/k8s/Controller/HelloController.java
+++ src/main/java/com/k8soperatorsapp/k8s/Controller/HelloController.java
@@ -1,13 +1,14 @@
-package com.k8soperators.k8s.Controller;
+package com.k8soperatorsapp.k8s.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
- 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello Word";  
-    }
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+
 }
